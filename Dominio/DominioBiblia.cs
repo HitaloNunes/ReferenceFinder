09c199de296c683c96545a8f2c9ac64db5d4f211
Hitalo Wingred <38959476+TioPatinhas00@users.noreply.github.com>
2021-10-21 11:35:25 -0300

    Ajustes

--- Dominio/DominioBiblia.cs
+++ Dominio/DominioBiblia.cs
@@ -15,7 +15,6 @@ namespace ReferenceFinder.Dominio
             Biblia retorno = new Biblia();
             List<Livro> livros = new List<Livro>();
             StreamReader text = new StreamReader("Assets\\acf.json");
-            //var tarefa = JsonConvert.DeserializeObject<Dictionary<string, string>>(text.ReadToEnd());
             livros = JsonConvert.DeserializeObject<List<Livro>>(text.ReadToEnd());
             retorno.Livros = livros.ToArray();
 
